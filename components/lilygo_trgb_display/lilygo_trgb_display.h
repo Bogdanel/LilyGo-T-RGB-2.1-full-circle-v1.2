7681d869b80de0c9856479517047c286700b09d3
Bogdan <bogdanelhh@gmail.com>
2025-11-08 13:36:26 +0200

    remove override

--- components/lilygo_trgb_display/lilygo_trgb_display.h
+++ components/lilygo_trgb_display/lilygo_trgb_display.h
@@ -5,13 +5,19 @@
 namespace esphome {
 namespace lilygo_trgb_display {
 
-class LilyGoTRGBDisplay : public display::DisplayBuffer, public Component {
+class LilyGoTRGBDisplay : public display::DisplayBuffer {
  public:
   void setup() override;
   void update() override;
   void draw_absolute_pixel_internal(int x, int y, Color color) override;
-  void flush_() override;
-  display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_RGB; }
+  void flush_();  // removed override keyword
+
+  display::DisplayType get_display_type() override {
+    return display::DisplayType::DISPLAY_TYPE_COLOR;
+  }
+
+  int get_width() override { return 480; }
+  int get_height() override { return 480; }
 };
 
 }  // namespace lilygo_trgb_display
