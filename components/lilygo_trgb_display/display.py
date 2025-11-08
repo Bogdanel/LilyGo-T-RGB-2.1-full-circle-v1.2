02f951670bcd0b84497eed25975b5b0de8d271eb
Bogdan <bogdanelhh@gmail.com>
2025-11-08 13:05:09 +0200

    add CONF_ID

--- components/lilygo_trgb_display/display.py
+++ components/lilygo_trgb_display/display.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import display
+from esphome.const import CONF_ID  # 👈 this line was missing
 from esphome import pins
 
 CODEOWNERS = ["@Bogdanel"]
@@ -10,7 +11,12 @@ LilyGoTRGBDisplay = lilygo_trgb_display_ns.class_(
     "LilyGoTRGBDisplay", display.DisplayBuffer, cg.Component
 )
 
-CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(LilyGoTRGBDisplay),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
