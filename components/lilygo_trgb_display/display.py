035c87e0446a10c8563b07b8449c10193fac4016
Bogdan <bogdanelhh@gmail.com>
2025-11-08 12:58:21 +0200

    COMPONENT UPDATE

--- components/lilygo_trgb_display/display.py
+++ components/lilygo_trgb_display/display.py
@@ -1,21 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import display
-from esphome.const import CONF_ID
+from esphome import pins
 
 CODEOWNERS = ["@Bogdanel"]
 
 lilygo_trgb_display_ns = cg.esphome_ns.namespace("lilygo_trgb_display")
 LilyGoTRGBDisplay = lilygo_trgb_display_ns.class_(
-    "LilyGoTRGBDisplay", display.DisplayBuffer
+    "LilyGoTRGBDisplay", display.DisplayBuffer, cg.Component
 )
 
-CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(LilyGoTRGBDisplay),
-    }
-).extend(cv.polling_component_schema("1s"))
-
+CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
